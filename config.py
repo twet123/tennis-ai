51da0650a939333b0a69502b366247170180b4ba
Vladimir Popov <vladapopov02@gmail.com>
2024-07-01 21:43:32 +0200

    feat: add additional config parameters

--- config.py
+++ config.py
@@ -12,3 +12,8 @@ class AgentParams:
         10000  # 10000 for testing/showcase, 10000000 for actual training
     )
     max_episodes = 10  # 10 for testing/showcase, more for actual training
+    epsilon_random_frames = 50000  # how many frames for taking random actions
+    epsilon_greedy_frames = 1000000.0  # how many frames for exploration
+    max_memory_length = 100000  # replay memory length
+    update_after_actions = 4  # train the model after this much actions
+    update_target_network = 10000
