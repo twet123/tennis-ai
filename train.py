310c3a91c33612ee546d1b58091ff4ab5bf3b7d1
Vladimir Popov <vladapopov02@gmail.com>
2024-07-03 14:09:38 +0200

    feat: implement model saving on keyboard interrupt

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import numpy as np
 import tensorflow as tf
 import h5py
 import os.path
+import pickle
 
 
 def load_h5(file_name):
@@ -21,141 +22,164 @@ def save_h5(file_name, arr):
 
 
 def train(env, actions, model, target_model):
-    optimizer = keras.optimizers.Adam(
-        learning_rate=AgentParams.learning_rate, clipnorm=1.0
-    )
-    loss_function = keras.losses.Huber()
+    print("Training...")
+    try:
+        optimizer = keras.optimizers.Adam(
+            learning_rate=AgentParams.learning_rate, clipnorm=1.0
+        )
+        loss_function = keras.losses.Huber()
+        if os.path.isfile("epsilon.pckl"):
+            with open("epsilon.pckl", "rb") as f:
+                AgentParams.epsilon = pickle.load(f)
+            print("usao", AgentParams.epsilon)
 
-    # replay memory
-    action_history = load_h5("action_history")
-    state_history = load_h5("state_history")
-    state_next_history = load_h5("state_next_history")
-    rewards_history = load_h5("rewards_history")
-    print(rewards_history)
-    done_history = load_h5("done_history")
-    print(done_history)
-    episode_reward_history = load_h5("episode_reward_history")
-    print(episode_reward_history)
+        # replay memory
+        action_history = load_h5("action_history")
+        state_history = load_h5("state_history")
+        state_next_history = load_h5("state_next_history")
+        rewards_history = load_h5("rewards_history")
+        done_history = load_h5("done_history")
+        episode_reward_history = load_h5("episode_reward_history")
 
-    running_reward = 0
-    episode_count = 0
-    frame_count = 0
+        running_reward = 0
+        episode_count = 0
+        frame_count = 0
 
-    while True:
-        observation, _ = env.reset()
-        state = np.array(observation)
-        episode_reward = 0
+        while True:
+            observation, _ = env.reset()
+            observation, reward, done, _, _ = env.step(1)  # fire on reset
+            state = np.array(observation)
+            episode_reward = 0
 
-        for timestep in range(1, AgentParams.max_steps_per_episode):
-            frame_count += 1
+            for timestep in range(1, AgentParams.max_steps_per_episode):
+                frame_count += 1
 
-            # exploration
-            if (
-                frame_count < AgentParams.epsilon_random_frames
-                or AgentParams.epsilon > np.random.rand(1)[0]
-            ):
-                action = np.random.choice(actions)
-            # exploitation
-            else:
-                state_tensor = keras.ops.convert_to_tensor(state)
-                state_tensor = keras.ops.expand_dims(state_tensor, 0)
-                action_probs = model(state_tensor, training=False)
-                action = keras.ops.argmax(action_probs[0]).numpy()
+                # exploration
+                if (
+                    frame_count < AgentParams.epsilon_random_frames
+                    or AgentParams.epsilon > np.random.rand(1)[0]
+                ):
+                    action = np.random.choice(actions)
+                # exploitation
+                else:
+                    state_tensor = keras.ops.convert_to_tensor(state)
+                    state_tensor = keras.ops.expand_dims(state_tensor, 0)
+                    action_probs = model(state_tensor, training=False)
+                    action = keras.ops.argmax(action_probs[0]).numpy()
 
-            AgentParams.epsilon -= (
-                AgentParams.epsilon_interval / AgentParams.epsilon_greedy_frames
-            )
-            AgentParams.epsilon = max(AgentParams.epsilon, AgentParams.epsilon_min)
-
-            state_next, reward, done, _, _ = env.step(action)
-            state_next = np.array(state_next)
-
-            episode_reward += reward
-
-            action_history.append(action)
-            state_history.append(state)
-            state_next_history.append(state_next)
-            done_history.append(done)
-            rewards_history.append(reward)
-            state = state_next
-
-            if (
-                frame_count % AgentParams.update_after_actions == 0
-                and len(done_history) > AgentParams.batch_size
-            ):
-                indicies = np.random.choice(
-                    range(len(done_history)), size=AgentParams.batch_size
+                AgentParams.epsilon -= (
+                    AgentParams.epsilon_interval / AgentParams.epsilon_greedy_frames
                 )
+                AgentParams.epsilon = max(AgentParams.epsilon, AgentParams.epsilon_min)
 
-                state_sample = np.array([state_history[i] for i in indicies])
-                state_next_sample = np.array([state_next_history[i] for i in indicies])
-                rewards_sample = [rewards_history[i] for i in indicies]
-                action_sample = [action_history[i] for i in indicies]
-                done_sample = keras.ops.convert_to_tensor(
-                    [float(done_history[i]) for i in indicies]
-                )
+                if reward == -1 or reward == 1:
+                    action = 1  # fire when point is starting
 
-                future_rewards = target_model.predict(state_next_sample)
-                # q value = reward + discount * expected future reward
-                updated_q_values = rewards_sample + AgentParams.gamma * keras.ops.amax(
-                    future_rewards, axis=1
-                )
+                state_next, reward, done, _, _ = env.step(action)
+                state_next = np.array(state_next)
 
-                updated_q_values = updated_q_values * (1 - done_sample) - done_sample
+                # if reward == 0:
+                #     reward -= 0.1
 
-                masks = keras.ops.one_hot(action_sample, actions)
+                episode_reward += reward
 
-                with tf.GradientTape() as tape:
-                    q_values = model(state_sample)
+                action_history.append(action)
+                state_history.append(state)
+                state_next_history.append(state_next)
+                done_history.append(done)
+                rewards_history.append(reward)
+                state = state_next
 
-                    q_action = keras.ops.sum(
-                        keras.ops.multiply(q_values, masks), axis=1
+                if (
+                    frame_count % AgentParams.update_after_actions == 0
+                    and len(done_history) > AgentParams.batch_size
+                ):
+                    indicies = np.random.choice(
+                        range(len(done_history)), size=AgentParams.batch_size
                     )
-                    loss = loss_function(updated_q_values, q_action)
 
-                    grads = tape.gradient(loss, model.trainable_variables)
-                    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+                    state_sample = np.array([state_history[i] for i in indicies])
+                    state_next_sample = np.array(
+                        [state_next_history[i] for i in indicies]
+                    )
+                    rewards_sample = [rewards_history[i] for i in indicies]
+                    action_sample = [action_history[i] for i in indicies]
+                    done_sample = keras.ops.convert_to_tensor(
+                        [float(done_history[i]) for i in indicies]
+                    )
 
-            if frame_count % AgentParams.update_target_network == 0:
-                target_model.set_weights(model.get_weights())
-                template = "running reward: {:.2f} at episode {}, frame count {}"
-                print(template.format(running_reward, episode_count, frame_count))
+                    future_rewards = target_model.predict(state_next_sample, verbose=0)
+                    # q value = reward + discount * expected future reward
+                    updated_q_values = (
+                        rewards_sample
+                        + AgentParams.gamma * keras.ops.amax(future_rewards, axis=1)
+                    )
 
-                model.save("tennis-model.keras")
-                save_h5("action_history", action_history)
-                save_h5("state_history", state_history)
-                save_h5("state_next_history", state_next_history)
-                save_h5("rewards_history", rewards_history)
-                save_h5("done_history", done_history)
-                save_h5("episode_reward_history", episode_reward_history)
+                    updated_q_values = (
+                        updated_q_values * (1 - done_sample) - done_sample
+                    )
 
-            if len(rewards_history) > AgentParams.max_memory_length:
-                del rewards_history[:1]
-                del state_history[:1]
-                del state_next_history[:1]
-                del action_history[:1]
-                del done_history[:1]
+                    masks = keras.ops.one_hot(action_sample, actions)
 
-            if done:
+                    with tf.GradientTape() as tape:
+                        q_values = model(state_sample)
+
+                        q_action = keras.ops.sum(
+                            keras.ops.multiply(q_values, masks), axis=1
+                        )
+                        loss = loss_function(updated_q_values, q_action)
+
+                        grads = tape.gradient(loss, model.trainable_variables)
+                        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+                if frame_count % AgentParams.update_target_network == 0:
+                    target_model.set_weights(model.get_weights())
+                    template = "running reward: {:.2f} at episode {}, frame count {}"
+                    print(template.format(running_reward, episode_count, frame_count))
+
+                    model.save("tennis-model.keras")
+                    with open("epsilon.pckl", "wb") as f:
+                        pickle.dump(AgentParams.epsilon, f)
+
+                if len(rewards_history) > AgentParams.max_memory_length:
+                    del rewards_history[:1]
+                    del state_history[:1]
+                    del state_next_history[:1]
+                    del action_history[:1]
+                    del done_history[:1]
+
+                if done:
+                    break
+
+            episode_reward_history.append(episode_reward)
+            if len(episode_reward_history) > 100:
+                del episode_reward_history[:1]
+            running_reward = np.mean(episode_reward_history)
+
+            episode_count += 1
+
+            if running_reward >= 24:
+                print("Solved at episode {}!".format(episode_count))
                 break
 
-        episode_reward_history.append(episode_reward)
-        if len(episode_reward_history) > 100:
-            del episode_reward_history[:1]
-        running_reward = np.mean(episode_reward_history)
-
-        episode_count += 1
-
-        if running_reward >= 24:
-            print("Solved at episode {}!".format(episode_count))
-            break
-
-        if (
-            AgentParams.max_episodes > 0 and episode_count >= AgentParams.max_episodes
-        ):  # Maximum number of episodes reached
-            print(
-                "Stopped at episode {}, running reward: {}!".format(
-                    episode_count, running_reward
+            if (
+                AgentParams.max_episodes > 0
+                and episode_count >= AgentParams.max_episodes
+            ):  # Maximum number of episodes reached
+                print(
+                    "Stopped at episode {}, running reward: {}!".format(
+                        episode_count, running_reward
+                    )
                 )
-            )
-            break
+                break
+    except KeyboardInterrupt:
+        model.save("tennis-model.keras")
+        with open("epsilon.pckl", "wb") as f:
+            pickle.dump(AgentParams.epsilon, f)
+
+        # save_h5("action_history", action_history)
+        # save_h5("state_history", state_history)
+        # save_h5("state_next_history", state_next_history)
+        # save_h5("rewards_history", rewards_history)
+        # save_h5("done_history", done_history)
+        # save_h5("episode_reward_history", episode_reward_history)
