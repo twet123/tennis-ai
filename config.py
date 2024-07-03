3743b4a6ca9e94dbc099945026276412ba08014c
Vladimir Popov <vladapopov02@gmail.com>
2024-07-03 14:08:57 +0200

    feat: modify training config

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ class AgentParams:
     max_steps_per_episode = (
         10000  # 10000 for testing/showcase, 10000000 for actual training
     )
-    max_episodes = 10000  # 10 for testing/showcase, 10000 more for actual training
+    max_episodes = 20000  # 10 for testing/showcase, 10000 more for actual training
     epsilon_random_frames = 50000  # how many frames for taking random actions
     epsilon_greedy_frames = 1000000.0  # how many frames for exploration
-    max_memory_length = 100000  # replay memory length
+    max_memory_length = 40000  # replay memory length
     update_after_actions = 4  # train the model after this much actions
     update_target_network = 10000
